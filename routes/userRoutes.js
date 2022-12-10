62735c644d89e1e0feeccfa43426ec7653d32b8b
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2022-12-11 02:46:46 +0300

    Add files via upload

---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const {
+  getUsers, creatUser, getUser, updateUser, updateUserAvatar,
+} = require('../controllers/user');
+
+router.get('/', getUsers);
+
+router.post('/', creatUser);
+
+router.get('/:id', getUser);
+
+router.patch('/me/avatar', updateUserAvatar);
+
+router.patch('/me', updateUser);
+
+const userRouter = router;
+
+module.exports = userRouter;
